8fa4221e3ab37e38791d40134819436c61e9d85e
Skylake-dev <77413867+Skylake-dev@users.noreply.github.com>
2022-03-31 11:56:01 +0200

    fix readthedocs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,6 +61,12 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
